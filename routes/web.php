396a68cece03c1fcab37005be0d4a2997b8f50ea
ReneGlaudemans <rra.glaudemans@student.avans.nl>
2024-08-25 23:31:59 +0200

    add receipt

--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\MenuController;
 use App\Http\Controllers\OrderController;
 use App\Http\Controllers\RestaurantController;
 use App\Http\Controllers\CustomerController;
+use App\Http\Controllers\ReceiptController;
 use Illuminate\Support\Facades\Route;
 
 Route::view('/', 'welcome');
@@ -21,3 +22,4 @@ Route::get('/order', [OrderController::class, 'viewOrder'])->name('order.view');
 Route::post('/order/update', [OrderController::class, 'updateOrder'])->name('order.update');
 Route::post('/order/remove', [OrderController::class, 'removeFromOrder'])->name('order.remove');
 Route::post('/order/store', [OrderController::class, 'store'])->name('order.store');
+Route::get('/receipt/{table}/pdf',[ReceiptController::class, 'exportToPDF'])->name('receipt.pdf');
